412091b64a515a748f93bc2536101698aedd15bf
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-02-19 22:27:13 +0800

    Refactoring

---
+++ src/containers/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/app.scss";
+
+class App extends Component {
+    render() {
+        return <div className="App"></div>;
+    }
+}
+
+export default App;
