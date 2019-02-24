57c68609fbca1db417be74d1f0efb0003a9f3af4
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-02-24 23:41:47 +0800

    updated loader component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,18 +3,49 @@ import "./loading.scss";
 
 interface LoadingProps {
     fullscreen?: boolean;
+    size?: number;
 }
 
 const loaderSize: number = 200;
 const loaderThickness: number = 10;
 
+const containerStyles: React.CSSProperties = {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    overflow: "hidden",
+    backgroundColor: "#eee",
+    zIndex: 99999,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+};
+
+function calculateStrokeSize(loaderSize: number): number {
+    return Math.ceil((loaderSize - (40 / 100) * loaderSize) / 10);
+}
+
 const Loading: React.FunctionComponent<LoadingProps> = (props: LoadingProps) => {
+    if (props.fullscreen !== undefined) {
+        if (!props.fullscreen) {
+            containerStyles.position = "absolute";
+        } else {
+            containerStyles.position = "fixed";
+        }
+    }
     return (
-        <div className="loading-container">
-            <svg xmlns="http://www.w3.org/2000/svg" width={loaderSize} height={loaderSize}>
+        <div
+            className="loading-container"
+            style={{
+                ...containerStyles,
+                position: props.fullscreen !== undefined ? (!props.fullscreen ? "absolute" : "fixed") : "fixed"
+            }}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width={props.size || loaderSize} height={props.size || loaderSize}>
                 <defs>
-                    <filter id="blur">
-                        <feGaussianBlur stdDeviation="5" />
+                    <filter id="blur" colorInterpolationFilters="sRGB" x="-1" y="-1" width="500%" height="500%">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
                     </filter>
                     <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
                         <stop offset="0%" stopColor="#ad00ad" />
@@ -22,20 +53,20 @@ const Loading: React.FunctionComponent<LoadingProps> = (props: LoadingProps) => 
                     </linearGradient>
                 </defs>
                 <circle
-                    cx="50%"
-                    cy="50%"
+                    x="50%"
+                    y="50%"
                     r="40%"
                     stroke="url(#linear)"
-                    strokeWidth={loaderThickness}
+                    strokeWidth={calculateStrokeSize(props.size || loaderSize)}
                     fill="transparent"
                     filter="url(#blur)"
                 />
                 <circle
-                    cx="50%"
-                    cy="50%"
+                    x="50%"
+                    y="50%"
                     r="40%"
                     stroke="url(#linear)"
-                    strokeWidth={loaderThickness}
+                    strokeWidth={calculateStrokeSize(props.size || loaderSize)}
                     fill="transparent"
                 />
             </svg>
